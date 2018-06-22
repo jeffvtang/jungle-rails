25548ab0dcff4df962a254d7cfd58d8d15042fe1
Jeff <jeff.tang@live.com>
2018-06-22 20:55:16 +0000

    can add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,16 +4,23 @@ class ReviewsController < ApplicationController
   #   @reviews = @reviews.where(product_id: params[:product]) if params[:product].present?
   #   @reviews = @reviews.where(user_id: params[:user]) if params[:user].present?
   # end
-
+  def new
+    @product = Product.find(params[:product_id])
+    @user = current_user.id
+    @review = Review.new(:product=>@product, :user=>@user)
+  end
+  
   def create
     @review = Review.new(review_params)
+    @review.save
+    
+    redirect_to :back
   end
-
 
   private
 
   def review_params
-    params.require(:review).permit(:description, :rating)
+    params.require(:review).permit(:description, :rating, :product_id, :user_id)
   end
 
 end
