0ad2eea15a74dec054bec4a5cc0998bff6ee56b5
jeffvtang <jeff.tang@gmail.com>
2018-06-22 18:46:12 +0000

    creating form for nested resource

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-    # @reviews = @reviews.where(product_id: params[:product]) if params[:product].present?
-    # @reviews = @reviews.where(user_id: params[:user]) if params[:user].present?
-  end
+  # def index
+  #   @reviews = Review.all
+  #   @reviews = @reviews.where(product_id: params[:product]) if params[:product].present?
+  #   @reviews = @reviews.where(user_id: params[:user]) if params[:user].present?
+  # end
 
   def create
-    review = Review.new(review_params)
+    @review = Product.reviews.new(review_params)
   end
 
 
