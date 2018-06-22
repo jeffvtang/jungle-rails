affb9a9ea04c8dc5b56f49c15a52d534c4e7d5e7
Jeff <jeff.tang@live.com>
2018-06-22 18:14:04 +0000

    made create route for reviews,

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:index, :show]
+    resources :reviews, only: [:index, :show, :create]
   end
   resources :categories, only: [:show]
 
