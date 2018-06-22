69ff24270ec70eb67092e25947c61bb481811eea
jeffvtang <jeff.tang@gmail.com>
2018-06-22 20:55:16 +0000

    can add reviews

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:create]
+    # resources :reviews
   end
   resources :categories, only: [:show]
 
