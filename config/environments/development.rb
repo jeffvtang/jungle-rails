2ce98a05b9aeeaafe3d92b0bad88ea894ee87fd7
Jeff <jeff.tang@live.com>
2018-06-22 13:50:31 +0000

    added admin auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,13 +17,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'jt.lhl.test',
-    password:             'tester123!',
-    authentication:       'plain',
-    enable_starttls_auto: true }
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['EMAIL_USER'],
+    :password             => ENV['EMAIL_PASS'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
+     
 
 
   # Print deprecation notices to the Rails logger.
