e9419c140bbd7025fecf12662767aa81c9e366d9
Jeff <jeff.tang@live.com>
2018-06-22 00:55:05 +0000

    added burner email for testing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
 
   def order_email(order, current_user)
     @order = order
-    @url  = 'http://example.com/login'
     mail(to: current_user.email, subject: 'Thank you for order. Order ID ' + order.id.to_s)
   end
 end
