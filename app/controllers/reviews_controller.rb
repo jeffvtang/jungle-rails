fef2bbc9a3b10f60b0a750e50fae433525f7897f
Jeff <jeff.tang@live.com>
2018-06-22 18:14:04 +0000

    made create route for reviews,

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,17 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
     # @reviews = @reviews.where(product_id: params[:product]) if params[:product].present?
     # @reviews = @reviews.where(user_id: params[:user]) if params[:user].present?
-
   end
+
+  def create
+    review = Review.new(review_params)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:review).permit(:description, :rating)
+  end
+
 end
