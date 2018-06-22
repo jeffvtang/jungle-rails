5420b29c7888d4d98413a2c2db2e9ea92c6397e8
Jeff <jeff.tang@live.com>
2018-06-22 17:35:51 +0000

    single review rendering

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def index
-    @reviews = Review.all
+    # @reviews = Review.all
     # @reviews = @product.reviews if @product.present?
-    @reviews = @reviews.where(product_id: params[:product]) if params[:product].present?
+    # @reviews = @reviews.where(product_id: params[:product]) if params[:product].present?
   end
 end
